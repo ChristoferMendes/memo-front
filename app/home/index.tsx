9c7b333fd04e36ea43862129edc89e425114a777
Christofer <christoferluizdsm@gmail.com>
2023-07-13 17:08:50 -0300

    feat: initial home cards

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,20 +1,28 @@
+import { SettingsIconButton } from '@components/SettingsIconButton';
 import { AntDesign } from '@expo/vector-icons';
-import { Text, View } from 'native-base';
+import { HStack, View } from 'native-base';
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { HomeCard } from '../../components/HomeCard';
 import { MainBackground } from '../../components/MainBackground';
+
+const cards = [
+  { title: 'Documents', icon: 'idcard' as const },
+  { title: 'Cards', icon: 'creditcard' as const },
+];
 
 export default function Home() {
   return (
     <View flex={1} bgColor="#1C1C1C">
       <MainBackground link="https://i.etsystatic.com/39519528/r/il/c3a97f/4486845485/il_570xN.4486845485_f2zb.jpg" />
-      <View>
-        <HomeCard title="Documents">
-          <AntDesign name="idcard" size={64} color="black" />
-        </HomeCard>
-      </View>
+      <HStack mx="12" mt="12" justifyContent="space-between" flexWrap="wrap">
+        {cards.map((card) => (
+          <HomeCard key={card.title} title={card.title}>
+            <AntDesign name={card.icon} size={64} color="black" />
+          </HomeCard>
+        ))}
+      </HStack>
+      <SettingsIconButton />
     </View>
   );
 }
