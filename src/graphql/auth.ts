fe29aea99dc9c3a2315ce4a1c8489eefac61b883
Christofer <christoferluizdsm@gmail.com>
2023-07-16 01:09:41 -0300

    added graphql fragment to core user fields

--- src/graphql/auth.ts
+++ src/graphql/auth.ts
@@ -1,12 +1,20 @@
 import { gql } from '@apollo/client';
 
+export const CORE_USER_FIELDS = gql`
+  fragment CoreUserFields on User {
+    id
+    name
+    email
+  }
+`;
+
 export const LOGIN = gql`
+  ${CORE_USER_FIELDS}
+
   mutation login($email: String!, $password: String!) {
     login(input: { email: $email, password: $password }) {
       user {
-        id
-        name
-        email
+        ...CoreUserFields
       }
       token
     }
@@ -14,21 +22,21 @@ export const LOGIN = gql`
 `;
 
 export const CREATE_USER = gql`
+  ${CORE_USER_FIELDS}
+
   mutation createUser($email: String!, $password: String!, $name: String!) {
     createUser(createUserInput: { email: $email, password: $password, name: $name }) {
-      id
-      name
-      email
+      ...CoreUserFields
     }
   }
 `;
 
 export const WHO_AM_I = gql`
+  ${CORE_USER_FIELDS}
+
   query whoami {
     me {
-      id
-      name
-      email
+      ...CoreUserFields
     }
   }
 `;
